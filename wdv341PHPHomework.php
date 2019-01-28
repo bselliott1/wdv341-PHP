ea13a007ec5e9f7fb0c185f89849a24340941e97
Brian Elliott <bselliott@dmacc.edu>
2019-01-27 22:42:04 -0600

    Add PHP Functions to repo

---
+++ wdv341PHPHomework.php
@@ -0,0 +1,23 @@
+<html>
+
+<body>
+
+<h1> Welcome to the homework page!</h1>
+
+<h1> Unit 1 - TestGit</h1>
+
+<p><a href = http://bselliott.us.tempcloudsite.com/wdv341/testGit.php> testGit.php</a></br>
+</p>
+
+<h1> Unit 2 - PHP Basics</h1>
+
+<p><a href = http://bselliott.us.tempcloudsite.com/wdv341/PHPBasics.php> PHP Basics</a></br>
+</p>
+
+<h1> Unit 3 - PHP Funactions</h1>
+
+<p><a href = http://bselliott.us.tempcloudsite.com/wdv341/PHP_Functions.php> PHP Functions</a></br>
+</p>
+
+</body>
+</html>
